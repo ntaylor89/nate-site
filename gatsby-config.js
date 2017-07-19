d4f4d47880b57a06481a606acf93833255593032
Nate Taylor <nathantaylor89@gmail.com>
2017-07-18 18:10:03 -0600

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `Nate A Taylor`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: { maxWidth: 740 }
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: { wrapperStyle: `margin-bottom 1.0725rem` }
+          }
+        ]
+      }
+    }
+  ],
+}
